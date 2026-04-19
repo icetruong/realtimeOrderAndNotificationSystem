cc5a4b596d424e3f645a6e75b2745a12c3603210
icetruong <hocthidauthoi@gmail.com>
2026-04-19 16:49:46 +0700

    add validation

--- src/main/java/com/ice/realtimecache/Auth/Controller/AuthController.java
+++ src/main/java/com/ice/realtimecache/Auth/Controller/AuthController.java
@@ -3,6 +3,7 @@ package com.ice.realtimecache.Auth.Controller;
 import com.ice.realtimecache.Auth.DTO.Request.LoginRequest;
 import com.ice.realtimecache.Auth.DTO.Request.RegisterRequest;
 import com.ice.realtimecache.Auth.Service.AuthService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +15,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public void register(@RequestBody RegisterRequest request)
+    public void register(@Valid @RequestBody RegisterRequest request)
     {
         authService.register(request);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request)
+    public String login(@Valid @RequestBody LoginRequest request)
     {
         return authService.verify(request);
     }
--- src/main/java/com/ice/realtimecache/Auth/DTO/Request/RegisterRequest.java
+++ src/main/java/com/ice/realtimecache/Auth/DTO/Request/RegisterRequest.java
@@ -1,10 +1,20 @@
 package com.ice.realtimecache.Auth.DTO.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
